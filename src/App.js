ff15bc7193421a29885556048730f1deb0ce3bde
yejin <ingkejin@gmail.com>
2022-04-26 01:53:22 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+// function App() {
+//   return (
+
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello,world!!!
+      </div>
+    );
+  }
+}
+
+export default App;
