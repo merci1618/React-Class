efe80624a14b1121f4d2c507e249b701c0023776
yejin <ingkejin@gmail.com>
2022-04-27 01:40:04 +0900

    🚧 Created new components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-// import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
+import './App.css';
 
-// function App() {
-//   return (
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        WORLD WIDE WEB!
 
-//   );
-// }
+        <nav>
+          <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">JAVASCRIPT</a></li>
+          </ul>
+        </nav>
+    </header>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello,world!!!
+        <Subject></Subject>
       </div>
     );
   }
