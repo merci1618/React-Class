30abc656c69969dd2d478b09a3b9a7076b0d0b4f
yejin <ingkejin@gmail.com>
2022-04-27 23:12:30 +0900

    🚧 Add new components

--- src/App.js
+++ src/App.js
@@ -7,15 +7,58 @@ class Subject extends Component {
       <header>
         <h1>WEB</h1>
         WORLD WIDE WEB!
+      </header>
+    );
+  }
+}
 
-        <nav>
-          <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="2.html">CSS</a></li>
-            <li><a href="3.html">JAVASCRIPT</a></li>
-          </ul>
-        </nav>
-    </header>
+class Toc extends Component {
+  render() {
+    return(
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JAVASCRIPT</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class ContentHtml extends Component {
+  render() {
+    return(
+      <article>
+        <h2>HTML</h2>
+        HTML IS HYPERTEXT MARKUP LANGUAGE.
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">HTML : MDN'S DOGS</a>
+        <button>PUSH</button>
+      </article>
+    );
+  }
+}
+
+class ContentCSS extends Component {
+  render() {
+    return(
+      <article>
+        <h2>CSS</h2>
+        CSS IS CASCADING STYLE SHEETS.
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">CSS : MDN'S DOGS</a>
+        <button>PUSH</button>
+      </article>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return(
+      <footer>
+        <p>@2022 GITHUB, INC.</p>
+        <a href="https://github.com/">https://github.com</a>
+      </footer>
     );
   }
 }
@@ -25,6 +68,14 @@ class App extends Component {
     return (
       <div className="App">
         <Subject></Subject>
+        <Toc></Toc>
+        <Toc></Toc>
+        <Toc></Toc>
+        <Toc></Toc>
+        <Toc></Toc>
+        <ContentHtml></ContentHtml>
+        <ContentCSS></ContentCSS>
+        <Footer></Footer>
       </div>
     );
   }
