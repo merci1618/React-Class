ce5dd4b0373548cda8b0ffb0174216910cdd731c
yejin <ingkejin@gmail.com>
2022-04-28 22:16:07 +0900

    🚧 Creating events for components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import Subject from './components/subject';
 import Toc from "./components/Toc"
 import Content from "./components/content"
-import Footer from './components/footer';
+// import Footer from './components/footer';
 import './App.css';
 
 // Componet가 실행 될 때 constructor라는 함수가 있다면 제일 먼저 실행 돼서 초기화를 담당한다.
@@ -13,9 +13,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mode:'WELCOME!',
+      mode:'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'WORLD WIDE WEB!!!'},
-      welcome:{title:'WELCOME', dese:'HELLO, REACT!'},
+      welcome:{title:'WELCOME', desc:'HELLO, REACT!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML IS HYPER TEXT MARKUP LANGUAGE.'},
         {id:2, title:'CSS', desc:'CSS IS CASCADING STYLE SHEETS.'},
@@ -27,36 +28,60 @@ class App extends Component {
   render() {
     console.log('App Render');
 
-    var _title, _dese = null;
+    var _title, _desc = null;
 
-    if(this.state.mode === 'WELCOME!') {
+    if(this.state.mode === 'Welcome') {
       _title = this.state.welcome.title;
-      _dese = this.state.welcome.dese;
+      _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _dese = this.state.contents[0].desc;
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
 
     return (
       <div className="App">
-        {/* <Subject 
-          title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
-        </Subject> */}
+        {/* Subject라는 component는 onChangePage라는 event가 있어서 component안에서 a link를 클릭 했을 때, evnet에 설치한 함수를 호출 할 수 있다. */}
+        <Subject 
+          title = {this.state.subject.title} 
+          sub = {this.state.subject.sub}
+          onChangePage = {function(){
+            this.setState({mode:'Welcome'});
+          }.bind(this)}
+          >
+        </Subject>
 
-        <header>
+        {/* App이라는 component 자체를 가르키는 객체를 해당 함수 안으로 주입해서 함수 안에서 this가 그 객체가 되도록 한다. => bind() */}
+        {/* <header>
           <h1><a href="/" onClick={function(e) {
             console.log(e);
             e.preventDefault();
-            alert('HI?');
-          }}>{this.state.subject.title}</a></h1>
+            // this.state.mode = 'WELCOME!'
+            this.setState({
+              mode:'Welcome'
+            });
+          }.bind(this)}>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}
-        </header>
+        </header> */}
 
-        <Subject title="REACT" sub="FOR UI"></Subject>
-        <Toc data={this.state.contents}></Toc>
-        <Content title={_title} desc={_dese}></Content>
-        <Footer></Footer>
+        <Toc 
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            });
+          }.bind(this)} 
+          data={this.state.contents}>
+        </Toc>
+        <Content title={_title} desc={_desc}></Content>
+        {/* <Footer></Footer> */}
       </div>
     );
   }
--- src/components/content.js
+++ src/components/content.js
@@ -7,8 +7,6 @@ class Content extends Component {
       <article>
         <h2>{this.props.title}</h2>
         {this.props.desc}
-        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">MDN'S DOGS</a>
-        <button>PUSH</button>
       </article>
     );
   }
